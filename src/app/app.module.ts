5219a5c3347a256bc17669d6774037a6cb28a3cf
Tomcio <tomasz.wolak@pl.ibm.com>
2017-08-06 03:22:19 +0200

    heroes.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,34 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { HeroDetailComponent } from './hero-detail.component';
-
+import {HeroesComponent} from './heroes.component'
 import { AppComponent }  from './app.component';
+import  { HeroService} from './hero.service'
+import {RouterModule } from '@angular/router';
+
+
+
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
-
+    FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    RouterModule.forRoot ([
+  {
+    path: 'heroes' ,
+    component: HeroesComponent
+  }
+])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
+  providers: [HeroService],
+
   bootstrap: [ AppComponent ]
+ 
 })
 export class AppModule { }
